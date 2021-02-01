4290bb383cac1967bc75786d7b5c81adcf569a8e
User <User@Mini-Desktop>
2021-01-31 21:14:40 -0700

    First Commit

---
+++ MadLibs_DavidYoung/src/MadLibs_DavidYoung.cpp
@@ -0,0 +1,49 @@
+//============================================================================
+// Name        : MadLibs_DavidYoung.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int name;
+	int favoriteNumber;
+	int noun1;
+	int noun2;
+	int noun3;
+	int noun4;
+	int noun5;
+	int noun6;
+	int adjective1;
+	int adjective2;
+	int verb;
+	int pluralNoun1;
+	int pluralNoun2;
+	int pluralNoun3;
+	int partOfBody1;
+	int partOfBody2;
+
+	cin >> name;
+	cin >> favoriteNumber;
+	cin >> noun1;
+	cin >> noun2;
+	cin >> noun3;
+	cin >> noun4;
+	cin >> noun5;
+	cin >> noun6;
+	cin >> adjective1;
+	cin >> adjective2;
+	cin >> verb;
+	cin >> pluralNoun1;
+	cin >> pluralNoun2;
+	cin >> pluralNoun3;
+	cin >> partOfBody1;
+	cin >> partOfBody2;
+
+
+	return 0;
+}
