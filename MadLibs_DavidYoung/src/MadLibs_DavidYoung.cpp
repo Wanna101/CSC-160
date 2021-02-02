00d9fa1561de362c8c59261a9a10a3349789af12
byoung <byoung@DESKTOP-PI2R9N9>
2021-02-01 22:18:38 -0700

    Refresh

--- MadLibs_DavidYoung/src/MadLibs_DavidYoung.cpp
+++ MadLibs_DavidYoung/src/MadLibs_DavidYoung.cpp
@@ -1,57 +1,23 @@
 //============================================================================
-// Name        : MadLibs_DavidYoung.cpp
+// Name        : Testing.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
-// Hello
 //============================================================================
 
+#include <string>
 #include <iostream>
 using namespace std;
 
 int main() {
-/*	int name;
-	int favoriteNumber;
-	int noun1;
-	int noun2;
-	int noun3;
-	int noun4;
-	int noun5;
-	int noun6;
-	int adjective1;
-	int adjective2;
-	int verb;
-	int pluralNoun1;
-	int pluralNoun2;
-	int pluralNoun3;
-	int partOfBody1;
-	int partOfBody2;
 
-	cin >> name;
-	cin >> favoriteNumber;
-	cin >> noun1;
-	cin >> noun2;
-	cin >> noun3;
-	cin >> noun4;
-	cin >> noun5;
-	cin >> noun6;
-	cin >> adjective1;
-	cin >> adjective2;
-	cin >> verb;
-	cin >> pluralNoun1;
-	cin >> pluralNoun2;
-	cin >> pluralNoun3;
-	cin >> partOfBody1;
-	cin >> partOfBody2;
-*/
+	string noun1, noun2, noun3, noun4, noun5, noun6, adjective1, adjective2, verb, adverb, pluralNoun1, pluralNoun2, pluralNoun3, bodyPart1, bodyPart2;
 
-	int name, favNum, noun1, noun2, noun3, noun4, noun5, noun6, adjective1, adjective2, verb, adverb, pluralNoun1, pluralNoun2, pluralNoun3, bodyPart1, bodyPart2;
-
-	cout << "Enter 6 nouns, 2 adjectives, 1 verb, 1 adverb, 3 plural nouns, and 2 body parts (in this order)."
+	cout << "Enter 6 nouns, 2 adjectives, 1 verb, 1 adverb, 3 plural nouns, and 2 body parts (in this order).";
 	cin >> noun1 >> noun2 >> noun3 >> noun4 >> noun5 >> noun6 >> adjective1 >> adjective2 >> verb >> adverb >> pluralNoun1 >> pluralNoun2 >> pluralNoun3 >> bodyPart1 >> bodyPart2;
 
-	cout << "Ye can always pretend to be a bloodthirsty " << noun1 << ", threatening everyone by waving yer " << adjective1 << " sword in the air, but until ye learn to " << verb << " like a pirate ye'll never be " << adverb << " accepted as an authentic " << noun << ". So here's what ye do: Cleverly work into yer daily conversations " << adjective2 << " pirate phrases such as \"Ahoythere, " << pluralNoun1 << ",\" \"Avast, ye " << pluralNoun2 << ",\" and \"Shiver me " << pluralNoun3 << ",\" Remember to drop all yer gs when ye say such words as sailin', spitten', and fightin'. THis will give ye a/an " << bodyPart1 << " start to being recognized as a swashbucklin' " << noun3 << ". Once ye have the lingo down pat, it helps to wear a three-cornered " << noun4 << " on yer head, stash a/an " << noun5 << " in yer pants, and keep a/an " << noun6 << " perched atop yer " << bodyPart2 << ". Aye, now ye be a real pirate!";
+	cout << "Ye can always pretend to be a bloodthirsty " << noun1 << ", threatening everyone by waving yer " << adjective1 << " sword in the air, but until ye learn to " << verb << " like a pirate ye'll never be " << adverb << " accepted as an authentic " << noun2 << ". So here's what ye do: Cleverly work into yer daily conversations " << adjective2 << " pirate phrases such as \"Ahoythere, " << pluralNoun1 << ",\" \"Avast, ye " << pluralNoun2 << ",\" and \"Shiver me " << pluralNoun3 << ",\" Remember to drop all yer gs when ye say such words as sailin', spitten', and fightin'. THis will give ye a/an " << bodyPart1 << " start to being recognized as a swashbucklin' " << noun3 << ". Once ye have the lingo down pat, it helps to wear a three-cornered " << noun4 << " on yer head, stash a/an " << noun5 << " in yer pants, and keep a/an " << noun6 << " perched atop yer " << bodyPart2 << ". Aye, now ye be a real pirate!";
 
 	return 0;
 }
