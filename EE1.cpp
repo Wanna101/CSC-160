84e9bce2c03a0d068bcb7c8f12b27a4d97ec7a4f
David Young <bryant.young@lumen.com>
2021-02-16 19:31:10 -0700

    update

---
+++ EE1.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "      !!!Hello!!!      " << endl;
+	cout << "My name is David Young." << endl;
+	return 0;
+}
---
+++ EE2.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : EE2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() { // initialize program
+	int x = 20; // initialize what x is equal to
+
+	if (x == 10) { // parameters if x is equal to 10 or not
+		cout << "x is 10" << endl;
+	} else {
+		cout << "x is not 10" << endl;
+	}
+	return 0;
+}
