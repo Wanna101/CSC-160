46d2d27e6959fe86f6972a63db2c16d0f683be78
David Young <dayyoung6165@gmail.com>
2021-02-18 19:36:09 -0700

    update from acer 315

--- WK04-RockPaperScissors.cpp
+++ WK04-RockPaperScissors.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : RockPaperScissors.cpp
-// Author      : 
+// Author      : David Young
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
